00556ccc68ad328196f182ef1619a198d872fe8b
simolex <simolex@yandex.ru>
2022-05-04 11:35:51 +0300

    Создали функцию validateValue и тесты для неё

---
+++ src/validateValue/validateValue.test.js
@@ -0,0 +1,19 @@
+const validateValue = require("./validateValue");
+
+describe("validateValue", () => {
+  test("Корректное значения", () => {
+    expect(validateValue(50)).toBe(true);
+  });
+  test("Пограничное значение снизу", () => {
+    expect(validateValue(0)).toBe(true);
+  });
+  test("Пограничное значение сверху", () => {
+    expect(validateValue(100)).toBe(true);
+  });
+  test("Меньше корректного значения", () => {
+    expect(validateValue(-1)).toBe(false);
+  });
+  test("Больше корректного значения", () => {
+    expect(validateValue(101)).toBe(false);
+  });
+});
